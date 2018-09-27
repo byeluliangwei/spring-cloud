c5117b766d5441e3f6bef73c59166a37a5809175
luliangwei <luliangwei1874@163.com>
2018-09-27 16:28:21 +0800

    基于ss官网的config demo

--- spring-cloud-user/src/main/java/cn/luliangwei/cloud/user/Application.java
+++ spring-cloud-user/src/main/java/cn/luliangwei/cloud/user/Application.java
@@ -1,13 +1,15 @@
 package cn.luliangwei.cloud.user;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
 public class Application 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
     }
+
 }
